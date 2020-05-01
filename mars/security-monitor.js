569cca6b2637da7e55fb8f191f20d0373d618a1b
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-05-01 21:27:30 +0200

    update security-monitor

--- mars/security-monitor.js
+++ mars/security-monitor.js
@@ -10,9 +10,21 @@ amqp.connect(config.amqp.url, function (error0, connection) {
             throw error1;
         }
 
-        var exchange = 'sensor-data';
+        //Empfangen der Daten
+        var exchange1 = 'sensor-data';
+        channel.assertExchange(exchange1, 'topic', {
+            durable: false
+        });
 
-        channel.assertExchange(exchange, 'topic', {
+        //Weiterleiten der Daten
+        var exchange2 = 'checked-data';
+        channel.assertExchange(exchange2, 'topic', {
+            durable: false
+        });
+
+        //Fehlermeldungen
+        var exchange3 = 'error';
+        channel.assertExchange(exchange3, 'topic', {
             durable: false
         });
 
@@ -24,14 +36,30 @@ amqp.connect(config.amqp.url, function (error0, connection) {
             }
             console.log(' [*] Waiting for data. To exit press CTRL+C');
 
-            channel.bindQueue(q.queue, exchange, '#');
+            channel.bindQueue(q.queue, exchange1, '#');
 
             channel.consume(q.queue, function (msg) {
                 console.log(" [x] Get data from " + msg.fields.routingKey);
+
                 //Anwendungslogik
+                //...
+
+                senddata(msg.fields.routingKey, msg.content, channel, exchange2)
+
             }, {
                 noAck: true
             });
         });
     });
+
+
+    function senddata(key, content, channel, exchange2) {
+        //Code zum weiterleiten
+        channel.publish(exchange, key, Buffer.from(content));
+    }
+
+    function senderror(key, content, channel, exchange3) {
+        //Code zum Senden einer Warnung
+        channel.publish(exchange, key, Buffer.from(content));
+    }
 })
