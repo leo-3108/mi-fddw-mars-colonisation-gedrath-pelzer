41a22dd0340ec749147946aee88d60361e4eaa23
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-05-01 21:28:17 +0200

    add sensor-aggregator

---
+++ mars/sensor-aggregator.js
@@ -0,0 +1,37 @@
+var amqp = require('amqplib/callback_api');
+var config = require('../_config/config.mars.json')
+
+amqp.connect(config.amqp.url, function (error0, connection) {
+    if (error0) {
+        throw error0;
+    }
+    connection.createChannel(function (error1, channel) {
+        if (error1) {
+            throw error1;
+        }
+
+        var exchange = 'aggregator-data';
+
+        channel.assertExchange(exchange, 'topic', {
+            durable: false
+        });
+
+        channel.assertQueue('', {
+            exclusive: true
+        }, function (error2, q) {
+            if (error2) {
+                throw error2;
+            }
+            console.log(' [*] Waiting for data. To exit press CTRL+C');
+
+            channel.bindQueue(q.queue, exchange, '#');
+
+            channel.consume(q.queue, function (msg) {
+                console.log(" [x] Get data from " + msg.fields.routingKey);
+                //Anwendungslogik
+            }, {
+                noAck: true
+            });
+        });
+    });
+})
