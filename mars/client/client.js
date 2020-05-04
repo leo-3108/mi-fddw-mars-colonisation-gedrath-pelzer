3a5b8f7c271d82e26673d736c6038252d6decdb7
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-05-04 14:52:20 +0200

    Change to Promises

--- mars/client/client.js
+++ mars/client/client.js
@@ -12,45 +12,52 @@ const config = require('../../_config/config.mars.json')
 
 // packages
 const amqp = require('amqplib')
-const logger = require('logging').default('Client')
+const logging = require('logging')
 const fs = require('fs').promises
 const uuid = require('uuid').v1
 const readline = require('readline')
 
-amqp.connect(config.amqp.url, function (error0, connection) {
-    if (error0) {
-        throw error0;
-    }
-    connection.createChannel(function (error1, channel) {
-        if (error1) {
-            throw error1;
+// create process objects
+const clientID = uuid()
+const output = logging.default('Client')
+const open = amqplib.connect(config.amqp.url)
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+
+// start process
+open.then(connection => {
+    return connection.createChannel()
+}).then(async channel => {
+
+    // Empfangen der Sensor-Daten
+    const sensor_exch = await channel.assertExchange(config.amqp.exch.sensor, 'topic', {
+        durable: false
+    })
+
+    // Empfangen der Fehlermeldungen
+    const aggregator_exch = await channel.assertExchange(config.amqp.exch.aggregator, 'topic', {
+        durable: false
+    })
+
+    channel.assertQueue('', {
+        exclusive: true
+    }, function (error2, q) {
+        if (error2) {
+            throw error2;
         }
+        console.log(' [*] Waiting for data. To exit press CTRL+C');
 
-        //Empfangen der Sensor-Daten
-        channel.assertExchange(config.amqp.exch.sensor, 'topic', {
-            durable: false
-        });
+        channel.bindQueue(q.queue, exchange1, '#');
 
-        //Empfangen der Fehlermeldungen
-        channel.assertExchange(config.amqp.exch.aggregator, 'topic', {
-            durable: false
-        });
-
-        channel.assertQueue('', {
-            exclusive: true
-        }, function (error2, q) {
-            if (error2) {
-                throw error2;
-            }
-            console.log(' [*] Waiting for data. To exit press CTRL+C');
-
-            channel.bindQueue(q.queue, exchange1, '#');
-
-            channel.consume(q.queue, function (msg) {
-                console.log(" [x] Get data from " + msg.fields.routingKey);
-            }, {
-                noAck: true
-            });
+        channel.consume(q.queue, function (msg) {
+            console.log(" [x] Get data from " + msg.fields.routingKey);
+        }, {
+            noAck: true
         });
     });
+}).catch(err => {
+    throw err
 })
