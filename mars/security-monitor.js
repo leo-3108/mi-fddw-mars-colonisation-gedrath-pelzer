c565a3aea2f48535d20162439e7a05a8279b8025
Leonard Pelzer <leonard.pelzer@smail.th-koeln.de>
2020-05-05 11:16:43 +0200

    add new exchange

--- mars/security-monitor.js
+++ mars/security-monitor.js
@@ -21,8 +21,7 @@ amqp.connect(config.amqp.url, function (error0, connection) {
         });
 
         //Fehlermeldungen
-        var exchenduser = config.amqp.exch.enduser;
-        channel.assertExchange(exchenduser, 'topic', {
+        var security_exch = channel.assertExchange(config.amqp.exch.security, 'topic', {
             durable: false
         });
 
@@ -32,18 +31,19 @@ amqp.connect(config.amqp.url, function (error0, connection) {
             if (error2) {
                 throw error2;
             }
-          
+
             channel.bindQueue(q.queue, sensor_exch.exchange, '#');
-          
+
             channel.consume(q.queue, function (msg) {
                 output.info('Get data from ' + msg.fields.routingKey + ' - ' + msg.content);
 
                 if (msg.content <= 20.5 || msg.content >= 24.5) {
-                    senderror(msg.fields.routingKey, msg.content, channel, exchenduser)
+                    senderror(msg.fields.routingKey, msg.content, channel, security_exch)
                 }
                 else {
-                    okay.info('Data okay - ' + msg.fields.routingKey)
+                    senddata(msg.fields.routingKey, msg.content, channel, security_exch)
                 }
+
 
             }, {
                 noAck: true
@@ -51,11 +51,17 @@ amqp.connect(config.amqp.url, function (error0, connection) {
         });
     });
 
-    function senderror(key, content, channel, exchenduser) {
+    function senddata(key, content, channel, exchange) {
+        //Code zum weiterleiten
+        channel.publish(exchange, key, Buffer.from(content));
+        okay.info('Sent data ' + key);
+    }
+
+    function senderror(key, content, channel, exchange) {
         //Code zum Senden einer Warnung
         var keytmp = key.split('.')
 
-        channel.publish(exchenduser, keytmp[0] + '.' + keytmp[1] + '.' + keytmp[2] + '.error', Buffer.from(content));
+        channel.publish(exchange, keytmp[0] + '.' + keytmp[1] + '.' + keytmp[2] + '.error', Buffer.from(content));
         error.info('Sent error - ' + keytmp[0] + '.' + keytmp[1] + '.' + keytmp[2] + '.error');
     }
 })
