ce0990f24858f023b2b670a79ea6195c52e3e720
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-05-05 15:05:18 +0200

    Send Data to Earth - Wait Time - Connects to Earth AMQP - assert Exchange - Send Data with same routing key and message

--- mars/interface.js
+++ mars/interface.js
@@ -12,11 +12,13 @@ const config_earth = require('../_config/config.mars.json')
 // packages
 const amqp = require('amqplib')
 const logging = require('logging')
+const util = require('util');
+const setTimeoutPromise = util.promisify(setTimeout);
 
 // create process objects
 const output = logging.default('Mars-Interface')
-const mars = amqp.connect(config.amqp.url)
-const earth = amqp.connect(config.amqp.url)
+const mars = amqp.connect(config_mars.amqp.url)
+const earth = amqp.connect(config_earth.amqp.url)
 
 
 // start process
@@ -25,7 +27,7 @@ mars.then(connection => {
 }).then(async channel => {
 
     // incoming messages
-    const enduser_exch = await channel.assertExchange(config.amqp.exch.enduser, 'topic', {
+    const mars_comm_exch = await channel.assertExchange(config_mars.amqp.exch.comm, 'topic', {
         durable: false
     })
 
@@ -36,7 +38,36 @@ mars.then(connection => {
         output.info("Started Mars-Interface - To exit press CTRL+C")
         
         // listen to communication exchange
+        channel.bindQueue(q.queue, mars_comm_exch.exchange, '#.normal')
 
+        // consume
+        channel.consume(q.queue, message => {
+
+            output.info('Message Received – Wait for 3 Secs')
+
+            // waiting simulates the travel time from mars to earth
+            setTimeoutPromise(60 * 3).then(async () => {
+
+                // connect to earth
+                await earth.then(earth_connection => {
+                    return earth_connection.createChannel()
+                }).then(async earth_channel => {
+
+                    const earth_comm_exch = await channel.assertExchange(config_earth.amqp.exch.comm, 'topic', {
+                        durable: false
+                    })
+
+                    if (channel.publish(exch.exchange, message.fields.routingKey, message.content))
+                        output.info("✅ Sent data to Earth 🌍 from " + message.fields.exchange)
+                    else
+                        output.error("Error accourd while sending data to Brocker")
+
+
+                }).catch(err => {
+                    throw err
+                })
+            })
+        })
 
     }).catch(err => {
         throw err
