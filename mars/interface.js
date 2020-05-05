985f550498f2947793ffb0240208f847d1d65b0d
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-05-05 14:44:00 +0200

    add base interface

--- mars/interface.js
+++ mars/interface.js
@@ -1,18 +1,46 @@
 /**
  * Mars-Interface
  * 
- * - Weiterleitung der Daten aus dem Erd-Netz in das Mars-Netz
+ * - Weiterleitung der Daten aus dem Mars-Netz in das Erd-Netz
  * - Simulation eines Delays
  */
 
 // config
-const config = require('../_config/config.mars.json')
-const config = require('../_config/config.mars.json')
+const config_mars = require('../_config/config.mars.json')
+const config_earth = require('../_config/config.mars.json')
 
 // packages
 const amqp = require('amqplib')
 const logging = require('logging')
 
 // create process objects
-const output = logging.default('Client')
-const open = amqp.connect(config.amqp.url)
+const output = logging.default('Mars-Interface')
+const mars = amqp.connect(config.amqp.url)
+const earth = amqp.connect(config.amqp.url)
+
+
+// start process
+mars.then(connection => {
+    return connection.createChannel()
+}).then(async channel => {
+
+    // incoming messages
+    const enduser_exch = await channel.assertExchange(config.amqp.exch.enduser, 'topic', {
+        durable: false
+    })
+
+    // establish own queue
+    await channel.assertQueue('', {
+        exclusive: true
+    }).then((q) => {
+        output.info("Started Mars-Interface - To exit press CTRL+C")
+        
+        // listen to communication exchange
+
+
+    }).catch(err => {
+        throw err
+    })
+}).catch(err => {
+    throw err
+})
