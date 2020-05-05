70005a9e0d1d88f5782c1dc0121821f7fbaecca7
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-05-05 11:22:29 +0200

    add basic moduls

--- mars/interface.js
+++ mars/interface.js
@@ -4,3 +4,15 @@
  * - Weiterleitung der Daten aus dem Erd-Netz in das Mars-Netz
  * - Simulation eines Delays
  */
+
+// config
+const config = require('../_config/config.mars.json')
+const config = require('../_config/config.mars.json')
+
+// packages
+const amqp = require('amqplib')
+const logging = require('logging')
+
+// create process objects
+const output = logging.default('Client')
+const open = amqp.connect(config.amqp.url)
