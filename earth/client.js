daaf5fff225da209be246f94c72b8ceb641f0613
Finn Nils Gedrath <fgedrath@th-koeln.de>
2020-05-06 18:32:45 +0200

    Finished Communication system (#34)
    * Add Basic Requiremenst
    
    * Interfaces für Erde und Mars (#24)
    
    * Unterstützt das Communication System, indem eine Schnittstelle geschaffen wurde
    * Grob gleiche Idee bei beiden:
      * Auf Nachrichten warten
      * Bei erhalt mit externem System verbinden
      * Nachricht mit gleichem Routing key weitersenden
    
    * Clients im Communiaction-System (#30)
    
    * Clients können Nachrichten zu ihrer ID empfangen
    * Client kann Nachrichten schicken. Syntax: m {empfänger_adresse} {nachricht}
    
    * Fixed som bugs
    
    * Add new Exchange enduser on earth
    Fixed einen Bug wo nachrichten im Loop gesendet wurden

--- earth/client.js
+++ earth/client.js
@@ -1,1 +1,118 @@
-//Client
+/**
+ * Client auf der Erde
+ * 
+ * - Mit spezifischem Client auf dem Mars kommunizieren.
+ */
+
+// config
+const config = require('../_config/config.earth.json')
+
+// packages
+const amqp = require('amqplib')
+const logging = require('logging')
+const shortid = require('shortid');
+const readline = require('readline')
+
+// packe settings
+shortid.characters('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ#$');
+
+// create process objects
+const clientID = shortid.generate()
+const output = logging.default('Earth-Client')
+const open = amqp.connect(config.amqp.url)
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+// start process
+open.then(connection => {
+    return connection.createChannel()
+}).then(async channel => {
+
+    // communication
+    const comm_exch = await channel.assertExchange(config.amqp.exch.comm, 'topic', {
+        durable: false
+    })
+
+    // receiving messages
+    const enduser_exch = await channel.assertExchange(config.amqp.exch.enduser, 'topic', {
+        durable: false
+    })
+
+    // establish own queue
+    await channel.assertQueue('', {
+        exclusive: true
+    }).then((q) => {
+        output.info("Started Earth-Client", clientID, "- To exit press CTRL+C")
+        output.info("[i] To Write a message write 'm {address} {message}'")
+
+        // listen to messages from mars
+        channel.bindQueue(q.queue, enduser_exch.exchange, clientID + '.normal')
+
+        // consume
+        channel.consume(q.queue, async message => {
+
+            if (message.content) {
+                let payload = JSON.parse(message.content.toString())
+                let date = new Date(payload.timestamp)
+
+                output.info(`📨 ${payload.from} schreibt um ${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}\n${payload.text}`)
+            }
+        }, {
+            // automatic acknowledgment mode,
+            // see https://www.rabbitmq.com/confirms.html for details
+            noAck: true
+        })
+
+        // readline
+        rl.on('line', input => {
+            let tmp = input.split(' ')
+
+            switch (tmp[0]) {
+                case 'm':
+                    send_message(channel, comm_exch, tmp[1], tmp.slice(2).join(' '))
+                    break;
+                default:
+                    output.error('First Argument must be one of the following: s, d')
+            }
+        });
+
+
+    }).catch(err => {
+        throw err
+    })
+}).catch(err => {
+    throw err
+})
+
+/**
+ * Sendet eine Nachricht an die Erde
+ * 
+ * @param {Channel} channel Current AMQP-Channel
+ * @param {Exchange} exch AMQP-Exchnage
+ * @param {String} address ID of recipient
+ * @param {String} message Message that will be sent to the recipient
+ */
+const send_message = (channel, exch, address, text) => {
+
+    if (shortid.isValid(address)) {
+        // create payload
+        let payload = {
+            timestamp: Date.now(),
+            from: clientID,
+            to: address,
+            text: text
+        }
+
+        // stop looking for topic
+        channel.publish(
+            exch.exchange,
+            address + '.normal',
+            Buffer.from(JSON.stringify(payload)))
+
+        output.info(`✅ Send message to: ${address}`);
+    } else {
+        output.error(`The Address "${address}" doesn't seem to be corret`)
+    }
+}
